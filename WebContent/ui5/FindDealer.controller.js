3d3e6ae04850ce8d49ccdcd402b10f7e3c4c16f4
davelap <dave_424242@hotmail.com>
2014-11-11 13:07:42 +0100

    Maps

--- WebContent/ui5/FindDealer.controller.js
+++ WebContent/ui5/FindDealer.controller.js
@@ -1,18 +1,18 @@
-sap.ui.controller("ui5.MainMenu", {
+sap.ui.controller("ui5.FindDealer", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf ui5.MainMenu
+* @memberOf ui5.FindDealer
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+		this.getView().byId("map_canvas").addStyleClass("myMap");
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf ui5.MainMenu
+* @memberOf ui5.FindDealer
 */
 //	onBeforeRendering: function() {
 //
@@ -21,30 +21,28 @@ sap.ui.controller("ui5.MainMenu", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf ui5.MainMenu
+* @memberOf ui5.FindDealer
 */
-//	onAfterRendering: function() {
-//
-//	},
+	onAfterRendering: function() {
+        if (!this.initialized) {
+            this.initialized = true;
+            this.geocoder = new google.maps.Geocoder();
+            var mapOptions = {
+                center: new google.maps.LatLng(-34.397, 150.644),
+                zoom: 8,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            this.map = new google.maps.Map(this.getView().byId("map_canvas").getDomRef(),
+                mapOptions);
+        }
+	}
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf ui5.MainMenu
+* @memberOf ui5.FindDealer
 */
 //	onExit: function() {
 //
 //	}
-	
-	// implement an event handler in the Controller
-	GoToPage2: function() {
-		app.to("idPage21");
-	},
 
-	OpenCarList: function() {
-		app.to("idPageCarList");
-	},
-	
-	OpenFindDealer: function() {
-		app.to("idPageFindDealer");
-	}
 });
