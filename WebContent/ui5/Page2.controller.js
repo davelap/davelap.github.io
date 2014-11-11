084f86731b0510ce41fb15641198fa7810125414
davelap <dave_424242@hotmail.com>
2014-11-11 11:48:49 +0100

    Save

--- WebContent/ui5/Page2.controller.js
+++ WebContent/ui5/Page2.controller.js
@@ -1,9 +1,9 @@
-sap.ui.controller("ui5.MainMenu", {
+sap.ui.controller("ui5.Page2", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf ui5.MainMenu
+* @memberOf ui5.Page2
 */
 //	onInit: function() {
 //
@@ -12,7 +12,7 @@ sap.ui.controller("ui5.MainMenu", {
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf ui5.MainMenu
+* @memberOf ui5.Page2
 */
 //	onBeforeRendering: function() {
 //
@@ -21,7 +21,7 @@ sap.ui.controller("ui5.MainMenu", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf ui5.MainMenu
+* @memberOf ui5.Page2
 */
 //	onAfterRendering: function() {
 //
@@ -29,14 +29,14 @@ sap.ui.controller("ui5.MainMenu", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf ui5.MainMenu
+* @memberOf ui5.Page2
 */
 //	onExit: function() {
 //
 //	}
 	
 	// implement an event handler in the Controller
-	GoToPage2: function() {
-		app.to("idPage21");
+	Back: function() {
+		app.back();
 	}
 });
