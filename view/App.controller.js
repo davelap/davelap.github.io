70cf9cf68a655cd69e8474e7acfe203f5bafdf4d
davelap <dave_424242@hotmail.com>
2014-11-29 15:57:26 +0000

    Full Change

---
+++ view/App.controller.js
@@ -0,0 +1,3 @@
+sap.ui.controller("root.view.App", {
+	
+});
---
+++ view/desktop/SalesOrder.controller.js
@@ -0,0 +1,47 @@
+sap.ui.controller("view.desktop.SalesOrder", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf ui5.desktop.SalesOrder
+*/
+	onInit: function() {
+        var oModel = new sap.ui.model.json.JSONModel("model/salesorder.json");
+        this.getView().setModel(oModel);
+        this.getView().bindElement("/SalesOrderCollection/0");
+        
+        // set i18n model
+        var i18nModel = new sap.ui.model.resource.ResourceModel({
+        bundleUrl : "i18n/sales.properties"
+        });
+        this.getView().setModel(i18nModel, "i18n");
+
+	}
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf ui5.desktop.SalesOrder
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf ui5.desktop.SalesOrder
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf ui5.desktop.SalesOrder
+*/
+//	onExit: function() {
+//
+//	}
+
+});
